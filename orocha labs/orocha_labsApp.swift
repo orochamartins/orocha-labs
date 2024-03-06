b97fed4420b9cd38027fb12a6095b2f7765e8872
Renato Martins <rochamartinsrenato@gmail.com>
2024-03-06 17:03:05 +0000

    Initial Commit

---
+++ orocha labs/orocha_labsApp.swift
@@ -0,0 +1,17 @@
+//
+//  orocha_labsApp.swift
+//  orocha labs
+//
+//  Created by Renato Martins on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct orocha_labsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
